a3dce46bcf4a67eb1a1cd6f2d4465cb6c2828c76
Matt <matt.garvin.416@gmail.com>
2023-11-11 18:37:25 -0500

    added HTML and CSS bones

---
+++ _INCLUDES/meta_head.php
@@ -0,0 +1,7 @@
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">			
+<link rel="stylesheet" type="text/css" href="<?php print $dots; ?>/css/default.css?t=<?php print $ver; ?>">
--- index.php
+++ index.php
@@ -1,5 +1,113 @@
 <?php
 
-    print "www.bestremoteworker.com";
+	// how deep is this page?
+	$dots = '.';
+	
+	// fetch the setup files!
+	include $dots . '/_PHP/setup.php';
 
-?>
+?><!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<head>
+<title>TEMPLATE</title>
+<meta name="Keywords" content="">
+<meta name="Description" content="">
+<?php include $dots . '/_INCLUDES/meta_head.php'; ?>
+<style>
+<!-- CUSTOM CSS FOR THIS PAGE -->
+		
+</style>
+</head>
+
+<body class="about">
+
+	<header>
+		<section>
+			HEADER
+		</section>	
+	</header>
+
+
+
+	<div id="hero">
+		<section>		
+			HERO
+		</section>	
+	</div>
+
+
+
+	<main>
+		<section class="bottom_gutter">		
+
+			<!-- ONE BOX -->
+			<div class="grid_1 top_gutter">
+				<div class="box">
+					One and only Box
+				</div><!-- end:box -->
+			</div><!-- end:grid_1 -->		
+
+			<!-- THREE BOXES -->
+			<div class="grid_3">
+				<div class="box">
+					1st Box
+				</div><!-- end:box -->
+
+				<div class="box">
+					2nd Box
+				</div><!-- end:box -->
+
+				<div class="box">
+					3rd Box
+				</div><!-- end:box -->
+			</div><!-- end:grid_3 -->		
+
+			<!-- TWO BOXES -->
+			<div class="grid_2">
+				<div class="box">
+					<h4 class="roboto"">BOXES and GRIDS</h4>
+					<ul> 
+						<li>boxes go in grids</li>
+						<li>both are <span class="code">&lt;div&gt;</span> tags</li>
+						<li>so this row has a 
+						<span class="code">&lt;div class="grid_2"&gt;</span> that 
+						contains two <span class="code">&lt;div class="box"&gt;</span> 
+						tags.</li>
+					</ul>
+				</div><!-- end:box -->
+				
+				
+				<div class="box">
+					2nd Box
+				</div><!-- end:box -->
+			</div><!-- end:grid_2 -->
+
+		</section>		
+	</main>
+
+
+
+	<footer>
+		<section>
+			FOOTER
+		</section>	
+	</footer>
+
+
+	<script
+		src="https://code.jquery.com/jquery-3.6.4.min.js"
+		integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+		crossorigin="anonymous"></script>
+	<script>
+
+		var ver = '<?php print $ver; ?>';
+		
+	</script>
+	<script src="<?php print $dots; ?>/js/default.js?t=<?php print $ver; ?>"></script>	
+	<script>
+	<!-- CUSTOM JS FOR THIS PAGE -->
+
+
+	</script>	
+</body>
+</html>
