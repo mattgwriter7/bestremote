a388a2d6751f889f9aced6c593f942176e5ce7bb
Matt <matt.garvin.416@gmail.com>
2023-11-15 11:54:14 -0500

    did a major overhaul with focus on mobile UI

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
 </style>
 </head>
 
-<body class="about">
+<body class="">
 
-	<header>
-		<section>
-			<span class="oswald">MATTGARVIN.COM</span>
+	<header class="ruler">
+		<section class="ruler">
+			<a name="top">SITE TITLE HERE</a>
 		</section>	
 	</header>
 
@@ -38,141 +38,255 @@
 
 
 	<main>
-		<section class="bottom_gutter">		
+		<section>		
 
-			<!-- ONE BOX -->
-			<div class="grid_1 top_gutter">
-			<?php include $dots . '/_BLURBS/welcome.htm'; ?>
-			</div><!-- end:grid_1 -->		
 
-			<!-- THREE BOXES -->
-			<div class="grid_3">
-				<div class="box img white">
-				  <div class="content">	
-					<img src="./images/mattgarvin.jpg"/>
-				  </div>	
-				</div><!-- end:box -->
 
-				<div class="box middle">
-				  <div class="content blue_bordered fadein">
-					<p><span class="oswald bigger">Guess What?</span>  I know my pic looks silly.  
-					It is meant to 
-					showcase my sense of humor! (And rugged good looks.)</p>
-					<p> I am passionate about creating 
-					<span class="roboto">COOL STUFF</span> 
-					but I can laugh at myself, too.
-				  </div>	
-				</div><!-- end:box -->
-
-				<div class="box">
-					<p class="dropcap">This site was made in about 
-						<span class="roboto">8&nbsp;HOURS</span> by Yours Truly.  I wanted to 
-						showcase my coding skills so I 
-						designed it from scratch, plus  
-						I stuck it  
-						on <span class="oswald">Github</span>. Want to have a look at the code?</p>
-						
-					<p>Check out the repo:<br />
-						<a href="https://github.com/mattgwriter7/bestremote" 
-						class="break_me" target="_blank">https://github.com/mattgwriter7/bestremote</a><br />
-						<div class="hint"><b>NOTE</b>: The repo is public, so it should be  
-							easy to peruse. (Or you can go Old School and 
-							just View Source!)
+			<div id="INTRO" class="panel">
+				<div class="interior">
+					
+					<h1>Hey Now!</h1>
+					<p class="dropcap">This is a paragraph tag with
+					a class="dropcap".  Dropcaps only look good if
+					there is a big block of text. (You don't want a
+					dangling dropcap if the paragraph is just one
+					short sentence!) To be honest, this paragraph is just
+					<i>barely</i> long enough.</p>
+					
+					<aside>
+						<img src="./images/mattgarvin.jpg"/>
+						<div>Guess What? I know my pic looks silly. 
+							It is meant to showcase my sense of humor! (And rugged 
+							good looks, obviously.)
 						</div>
-					</p>						
-				</div><!-- end:box -->
-			</div><!-- end:grid_3 -->		
+					</aside>
+					
+					<p>The "Hey Now!" is an &lt;h1&gt; tag.  It looks
+					all fancy because some CSS is applied to it.  These
+					"main heading" tags should be used judiciously. (I
+					am only using them at the start of "panels," which
+					I explain in a moment.)</p>
+					
+					<h2>Panels</h2>
+					<p>A "div.panel" is like a section that
+						can be shown or hidden.  They are denoted by
+						the a lot of vertical padding, plus
+						the little black dots up top.</p>
+						
+					<p>Panels always start with a "div.interior"
+						which is used to actually add the padding.</p>
+						
+					<p>This "Panel" section starts with an &lt;h2&gt; tag,
+						which (obviously) is a bit smaller than an
+						&lt;h1&gt; tag.  I don't use any headings
+						besides h1 and h2.<br />
+						<span class="note"><u>NOTE:</u> H1s and H2s
+							may have different margins and paddings when
+							used in a grid.
+						</span>
+					</p>		
 
-			<!-- ONE WIDE BOX -->
-			<div class="grid_1">
-				<div class="box wide">
-					<p class="dropcap">Here is the deal.  I have a university degree, and
-						a college diploma.  Yet, I am an "autodidact."  Why? Because I have
-						taught myself FAR more using the internet and my own
-						RESOURCEFULNESS than I ever learned in school.</p>
-					<p>I am one of those smug nerds with the ability become
-						quite knowledgable on almost any topic as long as I have an internet
-						connection, a couple hours, and the will to learn (which I do 99.97% of the time).</p>
-				</div><!-- end:box -->
-			</div><!-- end:grid_1 -->
-			
-			<!-- "scalable image" -->
-			<div class="scale_img">
-				<img src="./images/autodidact.gif" style="max-width: 600px; float: right;" />
-			</div><br class="clr"/>
+					<h2>Other Stuff</h2>
+					<p>I need to add an "aside" that will
+						float a div on the right, that can
+						have an image and a caption. (When it is
+						on mobile, it will be full-width.)</p>
+
+				</div><!-- end:interior -->
+			</div><!-- end:panel -->
 			
 			
 			
-			
-			
-			
+			<div id="FONTS" class="panel">
+				<div class="closed">
+					Do you want to see my<br />
+					<b class="r big">FONTS</b> write-up?<br />
+					<button class="show_me" data-panel="FONTS">show me!</button>
+					<div class="spinner"><br /></div>
+					<span class="spinner_mssg"></span>					
+				</div>	
+				
+				<div class="hud" id="main_show_me">Click the blue "show&nbsp;me!"
+					button to open up this panel.
+				</div>
+
+			</div><!-- end:panel -->
+
+
+
+
 			<div class="panel">
 			
-				<div class="closed" data-panel="REMOTE">
+				<div class="interior">
+					
+					<h1>Grids 'n Boxes</h1>
+					<p>Many things are displayed using Grids. To keep
+						things simple, a grid is always a row of "boxes". 
+						If there appears to be 6 same-sized boxes, 3 on
+						top of the other, it is actually two
+						different "div.grid_3" classes, holding
+						three different "div.box" containers, stacked.
+					</p>
+					
+					<h2>grid_3 Example</h2>
+					<p>This first example is what I just described: a
+						row with 3 boxes in it:</p>
+
+					<div class="grid_3">
+						<div class="box">
+							<ol>
+								<li>This is the first
+										"box" in a "grid_3"
+									<ul>
+										<li>by default grids have a 
+												margin-bottom
+										</li>
+										<li>Add a "short" class to
+												remove margin-bottom
+										</li>
+									</ul>											
+								</li>
+							</ol>			
+						</div>
+						<div class="box">
+							<ol start="2">
+								<li>Boxes are grey by
+										default
+									<ul>
+										<li>they also have padding on
+												all 4 sides by default
+										</li>
+										<li>plus, they are min-height
+												of 180px by default
+										</li>
+									</ul>												
+								</li>
+							</ol>			
+						</div>
+						<div class="box blank">
+							<ol start="3">
+								<li>This 3rd one appears
+										white because the
+										class is "blank"
+									<ul>
+										<li>blanks also have padding
+												removed
+										</li>
+										<li class="note"><u>NOTE</u>: There is 
+												still a margin-bottom on the
+												grid itself!
+										</li>
+									</ul>										
+								</li>
+							</ol>	
+						</div>	
+					</div><!-- end this grid row -->
+
+					<h2>grid_2 Example</h2>
+
+					<div class="grid_2">
+						<div class="box">
+							<ol start="4">
+								<li>This is the 4th "box"
+									<ul>
+										<li>It is in a "grid_2"</li>
+									</ul>	
+								</li>
+							</ol>							
+						</div>
+						<div class="box">
+							<ol start="5">
+								<li>This is the 5th "box"
+									<ul>
+										<li>The &lt;h2&gt; tag has a class="tall" applied 
+											add a margin-bottom after the heading
+										</li>
+										<li>In general, a "tall" class adds vertical
+											spacing, a "short" class removes it.
+										</li>
+										<li class="note"><u>NOTE</u>: Not
+											just for headings, but for many things,
+											including grids. (See Box #6 below.)		
+										</li>
+									</ul>	
+								</li>
+							</ol>						
+						</div>						
+					</div><!-- end this grid row -->
+
+					<h2>grid_1 Example</h2>
+					
+					<div class="grid_1 short">
+						<div class="box">
+							<ol start="6">
+								<li>This is the 6th "box"
+									<ul>
+										<li>It is in a "grid_1"</li>
+										<li>The grid has a "short" class that
+												makes it have no margin-bottom
+										</li>
+										<li>...which makes it butt up
+												against the grid_3 underneath
+										</li>
+									</ul>	
+								</li>
+							</ol>							
+						</div>
+					</div><!-- end this grid row -->
+
+					<div class="grid_3">
+						<div class="box">
+							<ol start="7">
+								<li>This is the 7th "box"</li>
+							</ol>	
+						</div>
+						<div class="box">
+							<ol start="8">
+								<li>This is the 8th "box"</li>
+							</ol>	
+						</div>
+						<div class="box">
+							<ol start="9">
+								<li>This is the 9th "box"</li>
+							</ol>	
+						</div>						
+					</div><!-- end this grid row -->
+
+					<h2>Other Box Examples?</h2>
+					
+					<p>I still need some fancy box stuff... Including:</p>
+					
+					<ol>
+						<li class="tall">A box that will center text (vertically 
+							and horizontally.)</li>
+						<li class="tall">Another one that will center an image (vertically 
+							and horizontally.)</li>
+						<li class="tall">Another one that will show an image
+							in the background, with a caption overlayed
+							at the bottom.</li>
+						<li class="tall">Finally, one that shows an image up
+							top with captioned text underneath
+							<div class="note">
+							By the way, these &lt;li&gt; tags have class="tall" 
+							added to bump up the margin-bottom.</div></li>
+					</ol>
+						
+				</div><!-- end:interior -->
+			</div><!-- end:panel -->	
+			
+
+
+			<div id="WRITING" class="panel">
+			
+				<div class="closed">
 					Do you want to see my<br />
-					REMOTE WORKER write-up?<br />
-					<button>Yes!</button>
+					<b class="r">WRITER</b> write-up?<br />
+					<button class="show_me" data-panel="WRITER">show me!</button>
+					<div class="spinner"><br /></div>
+					<span class="spinner_mssg"></span>					
 				</div>	
 
 			</div><!-- end:panel -->
-
-
-
-
-
-			<div class="panel">
-			
-				<h1>MY WRITING</h1>
-				
-
-				<!-- TWO BOXES -->
-				<div class="grid_2">
-					<div class="box white">
-						<h2 class="roboto">RECENT NEWS</h2>
-						<p>Three years ago I started doing MOBILE&nbsp;DEVELOPMENT 
-						using 
-						<span class="roboto">FLUTTER</span>
-						for FLUID&nbsp;MOBILITY.  
-							I soon came to realize I LOVE FLUTTER!</p> 
-						<p>Please read my article on Medium:<br /> 
-						"5 Things I Love About Flutter"	
-					</div><!-- end:box -->
-					
-					<div class="box white">
-						<h2 class="roboto">SHORT BIO</h2>
-						<p>A loooong time ago I started my career as a 
-							Graphic&nbsp;Designer. Then I became a Web&nbsp;Developer. 
-							Next I became a UI&nbsp;Designer, and 
-							most recently a Mobile&nbsp;Developer.</p>  
-						<p>I am a "Jack-of-all-trades" and... well 
-							 I am pretty decent at a few of them.</p>		
-					</div><!-- end:box -->
-				</div><!-- end:grid_2 -->
-
-
-				<!-- TWO MORE BOXES -->
-				<div class="grid_2">
-					<div class="box">
-						<h4 class="roboto"">BOXES and GRIDS</h4>
-						<ul> 
-							<li>boxes go in grids</li>
-							<li>both are <span class="code">&lt;div&gt;</span> tags</li>
-							<li>so this row has a 
-							<span class="code">&lt;div class="grid_2"&gt;</span> that 
-							contains two <span class="code">&lt;div class="box"&gt;</span> 
-							tags.</li>
-						</ul>
-					</div><!-- end:box -->
-					
-					
-					<div class="box">
-						2nd Box
-					</div><!-- end:box -->
-				</div><!-- end:grid_2 -->
-
-			</div><!-- end:panel -->
-			
-			
 			
 			
 			<div class="panel">
@@ -192,7 +306,20 @@
 
 	<footer>
 		<section>
-			FOOTER
+			<div class="grid_3">
+				<div class="box">
+					FOOTER BOX 1<br />
+					(Contact)
+				</div>
+				<div class="box">
+					FOOTER BOX 2<br />
+					(Social Media)
+				</div>
+				<div class="box">
+					FOOTER BOX 3<br />
+					(??)
+				</div>						
+			</div><!-- end this grid row -->			
 		</section>	
 	</footer>
 
--- js/default.js
+++ js/default.js
@@ -1,18 +1,23 @@
 
-// 	LEFT OFF: only just began
+// 	LEFT OFF: I added spinner and fetch
+//	NEXT: 
 
 $(function() {
 	
 	// 	===========================================	
+	//	MAIN FUNCTIONS
+	// 	===========================================		
 
 	function init() {
 		console.log( 'Loaded version ' + window.ver );
     
+		/*
 		setTimeout(function(){ 
 			$( '.fadein' ).fadeIn( 'fast', function() {
 				// Animation complete
 			});		
 		}, 2000);
+		*/
 		
 		return;
 	}
@@ -20,6 +25,23 @@ $(function() {
 	// 	===========================================	
 	//	EVENT HANDLERS
 	// 	===========================================	
+
+	$( 'button' ).click(function(){
+		var panel = $(this).attr('data-panel');
+		//	update spinner
+		$( '#' + panel + ' .spinner' ).addClass('active');
+		$( '#' + panel + ' .spinner_mssg' ).text('fetching...');
+		//	hide HUD
+		$( '#main_show_me' ).fadeOut();
+		setTimeout(() => {
+			//	load the panel content
+			//	WILLFIX: ?? make this full on AJAX so I can
+			//	add a slideDown ?? maybe, maybe not...
+			$( '#' + panel ).load( './' + panel + '.php',function(data){
+				
+			});			
+		}, 2000);		
+	});
 
 	// 	===========================================
 	//	INITIALIZE 
